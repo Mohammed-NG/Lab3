d35100608fa43d30aa1d89964d6f01918b13e1f6
Ali10rma <alialabduljabbar0@gmail.com>
2023-02-11 22:34:19 +0300

    create instertion sort calling method

--- Sorter.java
+++ Sorter.java
@@ -18,22 +18,24 @@ public class Sorter {
     {
         int n = arr.length;
 
-        // One by one move boundary of unsorted subarray
+
         for (int i = 0; i < n-1; i++)
         {
-            // Find the minimum element in unsorted array
+
             int min_idx = i;
             for (int j = i+1; j < n; j++){
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
             }
 
-            // Swap the found minimum element with the first
-            // element
+
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
             arr[i] = temp;
         }
+    }
+    public void sort(int array[] ){
+        insertionSort(array);
     }
 
 }
